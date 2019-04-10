9f69623db2b5f49e8800e8ccfb3c12be45fcb22e
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2019-04-10 12:20:21 +0300

    init

---
+++ AnonRecords/Library.fs
@@ -0,0 +1,5 @@
+﻿module AnonRecords.M
+
+type AR = {| I: int; S: string |}
+
+let getAR () = {| I = 1; S = "" |}
---
+++ ConsoleApp/Program.fs
@@ -0,0 +1,8 @@
+﻿
+[<EntryPoint>]
+let main _ =
+    
+    let f (_: {| I: int; S: string |}) = ()
+
+    f (AnonRecords.M.getAR())
+    0
