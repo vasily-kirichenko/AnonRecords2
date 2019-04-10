c7f920c125c218ea41affcd90970b2e133d80857
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2019-04-10 12:45:53 +0300

    another bug

--- AnonRecords/Library.fs
+++ AnonRecords/Library.fs
@@ -3,3 +3,8 @@
 type AR = {| I: int; S: string |}
 
 let getAR () = {| I = 1; S = "" |}
+
+type R = { F: float; AR: AR }
+
+let getR () = { F = 1.0; AR = getAR() }
+
--- ConsoleApp/Program.fs
+++ ConsoleApp/Program.fs
@@ -1,8 +1,15 @@
 ﻿
 [<EntryPoint>]
 let main _ =
-    
-    let f (_: {| I: int; S: string |}) = ()
+    let f (_: AnonRecords.M.AR) = ()
 
-    f (AnonRecords.M.getAR())
+    // Commenting this line results with the compiler crashing with:
+    // Error the anonymous record <>f__AnonymousType611736317`2' has not been generated in the pre-phase of generating this module	ConsoleApp	C:\WLCR\github\AnonRecords\ConsoleApp\error FS0192	1	
+    f (AnonRecords.M.getAR()) 
+
+    let ar: AnonRecords.M.AR = {| I = 1; S = "" |}
+    f ar
+
+    f (AnonRecords.M.getR().AR)
+
     0
